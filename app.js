a619a7116c024dafbc91cfe360cca22a71b8bef2
camille <camille@cstar.io>
2021-08-14 02:23:52 +0200

    prettier

--- app.js
+++ app.js
@@ -1,51 +1,50 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 //Appel des variables d'environnement
-const dotEnv = require('dotenv').config();
+const dotEnv = require("dotenv").config();
 
-//Base de donnée mySql
-//plutot mysql chercher des informations sur mysql2 et mysql comparaison plus simple plus moderne et rapide.
-const mysql = require('mysql2');
-
-//Connection à la base de donnée
-const mysqlConnection = require('./connection/connection-db');
+//Multer
+const multer = require("multer");
 
 //Helmet protection
 const helmet = require("helmet");
 
 //ROUTES
-const postsRoutes = require('./routes/postsRoutes');
-const userRoutes = require('./routes/userRoutes');
+const postsRoutes = require("./routes/postsRoutes");
+const userRoutes = require("./routes/userRoutes");
 // const responsesRoutes = require('./routes/responsesRoutes');
-
-
 
 //Application express
 const app = express();
-
-//Body parser
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.json());
 
 //Helmet sécurisation
 app.use(helmet());
 
 // securisation avec les autorisation d'en tete
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use(express.json());
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/api/user', userRoutes);
-app.use('/api/post', postsRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/api/user", userRoutes);
+app.use("/api/post", postsRoutes);
 // app.use('api/responses', responsesRoutes);
 
 module.exports = app;
+
+//Commenter ttes les lignes mysql et db la connection se fait a partir de sequelize !!!
--- models/post.js
+++ models/post.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Post extends Model {
     /**
@@ -9,28 +7,31 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    
+
     static associate(models) {
       // define association here
       models.Post.belongsTo(models.Post, {
         foreignKey: {
           allowNull: true,
-        }
+        },
       });
       models.Post.belongsTo(models.User, {
         foreignKey: {
-          allowNull: false
-        }
+          allowNull: false,
+        },
       });
     }
-  };
-  Post.init({
-    title: DataTypes.STRING,
-    content: DataTypes.STRING,
-    attachement: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Post',
-  });
+  }
+  Post.init(
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      attachement: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Post",
+    }
+  );
   return Post;
 };
