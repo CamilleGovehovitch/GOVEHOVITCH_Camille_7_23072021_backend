1e4ca4458d2011e346a8d1f1b5051641873217e0
camille <camille@cstar.io>
2021-08-14 02:22:26 +0200

    add multer post put routes

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,37 +1,33 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-//Connection My Sql
-const mysqlConnection = require('../connection/connection-db');
-
 //Authentification
-const auth = require('../middlewares/auth');
+const auth = require("../middlewares/auth");
 
 //Export de fichier
-const multer = require('../middlewares/multer-config');
+const multer = require("../middlewares/multer-config");
 
 //Controllers
-const postsController = require('../controllers/postsController');
-const responsesController = require('../controllers/responsesController');
+const postsController = require("../controllers/postsController");
+const responsesController = require("../controllers/responsesController");
 
 //Posts Routes et Controllers
-router.get('/', auth, postsController.getPosts);
-router.get('/:id', auth, postsController.getPost);
-router.post('/new', auth, postsController.createPost);
-router.put('/:id/edit', auth, postsController.modifyPost);
-router.delete('/:id', auth, postsController.deletePost);
+router.get("/", auth, postsController.getPosts);
+router.get("/:id", auth, postsController.getPost);
+router.post("/new", auth, multer, postsController.createPost);
+router.put("/:id/edit", auth, multer, postsController.modifyPost);
+router.delete("/:id", auth, postsController.deletePost);
 
 //Post-response Routes et Controllers
-router.post('/:id/new', responsesController.createResponse);
+router.post("/:id/new", responsesController.createResponse);
 
 //Likes Dislikes Routes et Controllers
-router.post('/:id/like', auth, postsController.like);
-router.delete('/:id/like', auth, postsController.unlike);
-router.post('/:id/dislike', auth, postsController.dislike);
-router.delete('/:id/dislike', auth, postsController.undislike);
+router.post("/:id/like", auth, postsController.like);
+router.delete("/:id/like", auth, postsController.unlike);
+router.post("/:id/dislike", auth, postsController.dislike);
+router.delete("/:id/dislike", auth, postsController.undislike);
 
-router.get('/:id', responsesController.getResponses);
+router.get("/:id", responsesController.getResponses);
 
 module.exports = router;
- 
